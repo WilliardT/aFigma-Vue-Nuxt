5dff626da75c36bb9e6d034289c028a8f1df5f7c
WilliardT <609353@bk.ru>
2023-01-28 21:19:22 +0300

    2119

--- components/RegisterForm.vue
+++ components/RegisterForm.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="form-section d-flex justify-content-center align-center">
-    <v-col>
-      <h2>Try Figma for free</h2>
-      <v-form v-model="valid">
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="e-mail"
-          required
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="password"
-          :rules="passwordRules"
-          label="Password"
-          required
-        >
-        </v-text-field>
-        <v-btn depressed color="blue"> Create acc </v-btn>
-      </v-form>
-    </v-col>
-  </div>
+  <v-row justify="center">
+    <div class="form-section d-flex justify-content-center align-center">
+      <v-col>
+        <h2>Try Figma for free</h2>
+        <v-form v-model="valid">
+          <v-col align="center">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="e-mail"
+              required
+            >
+            </v-text-field>
+            <v-text-field
+              v-model="password"
+              :rules="passwordRules"
+              label="Password"
+              required
+            >
+            </v-text-field>
+            <v-btn outlined color="black"> Create account </v-btn>
+          </v-col>
+          <p align="center">Already have an account <a>Log in</a></p>
+          <small>By clicking "Create account", I agree to Figma's TOS and Privacy Policy.</small>
+        </v-form>
+      </v-col>
+    </div>
+  </v-row>
 </template>
 
 <script>
@@ -41,7 +47,7 @@ export default {
     password: '',
     passwordRules: [
       (v) => !!v || 'Password is required',
-      (v) => v.length > 6 || 'Password must be valid',
+      (v) => v.length > 6 || 'Password must not be less than 6 chars',
     ],
   }),
 }
@@ -50,5 +56,6 @@ export default {
 <style scoped>
 .form-section {
   height: 95vh;
+  max-width: 400px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,20 +10,20 @@
           autoplay=""
           muted=""
           playsinline=""
-          style="max-width:100%; width: 100vw; height: 35vh;"
-        >
-        </video>
+          style="max-width: 70%"
+        ></video>
         <h3>
-          Figma connects everyone in the design process so teams can deliver better products, faster.
+          Figma connects everyone in the design process so teams can deliver
+          better products, faster.
         </h3>
-        <v-btn depressed color="primary">
-          Sign up free
-        </v-btn>
+        <br />
+        <v-btn depressed color="primary"> Sign up free </v-btn>
+        <br />
       </v-col>
     </v-container>
-    <v-img 
+    <v-img
       src="https://cdn.sanity.io/images/599r6htc/localized/455e29e0d05e64300b4a2aa99bcd25fa10a341a7-6969x2010.png?rect=0,1,6969,2008&w=2016&h=581&q=75&fit=max&auto=format"
-      style="width: 200%" 
+      style="width: 200%"
     />
     <v-container>
       <SocialCard />
